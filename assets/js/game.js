1e40609e5218f8101ded18a420c81426c74a5530
johndavis <johndavis92790@gmail.com>
2021-11-06 11:18:53 -0600

    for statement work

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,7 +4,7 @@ var playerAttack = 10;
 var playerMoney = 20;
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
-var enemyHealth = 50;
+var enemyHealth = 30;
 var enemyAttack = 12;
 
 // You can also log multiple values at once like this
@@ -86,7 +86,24 @@ var fight = function(enemyName) {
 for(var i = 0; i < enemyNames.length; i++) {
     // debugger;
     // call fight function with enemy-robot
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
+    if (playerHealth > 0) {
+        // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+    
+        // pick new enemy to fight based on the index of the enemyNames array
+        var pickedEnemyName = enemyNames[i];
+    
+        // reset enemyHealth before starting new fight
+        enemyHealth = 50;
+    
+        // use debugger to pause script from running and check what's going on at that moment in the code
+        // debugger;
+    
+        // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+        fight(pickedEnemyName);
+    }
+    else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+    }
 }
