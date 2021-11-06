1d8b23d1ebe1c53fccf67d5b38472c78754c2f46
johndavis <johndavis92790@gmail.com>
2021-11-06 15:35:19 -0600

    save high score on local storage

--- assets/js/game.js
+++ assets/js/game.js
@@ -124,13 +124,13 @@ var getPlayerName = function() {
 
 var playerInfo = {
     name: getPlayerName(),
-    health: 100,
-    attack: 10,
+    health: 1000,
+    attack: 50,
     money: 10,
     reset: function() {
-      this.health = 100;
+      this.health = 1000;
       this.money = 10;
-      this.attack = 10;
+      this.attack = 50;
     }, // comma!
     refillHealth: function() {
         if (this.money >= 7) {
@@ -226,22 +226,30 @@ var startGame = function() {
 
 //function to end the entire game
 var endGame = function() {
-    // if player is still alive, player wins!
-    if (playerInfo.health > 0) {
-      window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
+    window.alert("The game has now ended. Let's see how you did!");
+  
+    // check localStorage for high score, if it's not there, use 0
+    var highScore = localStorage.getItem("highscore");
+    highScore = highScore || 0;
+    // if player has more money than the high score, player has new high score!
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+  
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
     } 
     else {
-      window.alert("You've lost your robot in battle.");
+        alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
     }
+  
     // ask player if they'd like to play again
     var playAgainConfirm = window.confirm("Would you like to play again?");
-
+  
     if (playAgainConfirm) {
-    // restart the game
-    startGame();
+        startGame();
     } 
     else {
-    window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!");
     }
 };
 var shop = function() {
